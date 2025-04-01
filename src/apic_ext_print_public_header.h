8e3ba1a80c0828565686df6b96a70d3738fafa9a
mg <morew4rd@gmail.com>
2025-03-31 17:12:43 -0700

    new ext: public header

---
+++ src/apic_ext_print_public_header.h
@@ -0,0 +1,136 @@
+#ifndef APIC_EXT_PRINT_PUBLIC_HEADER_H
+#define APIC_EXT_PRINT_PUBLIC_HEADER_H
+
+#define APIC_REFLECT
+#include "apic.h"
+
+#include <stdio.h>
+
+static void __impl_ext_print_public_header(Exports *exports, FILE *out);
+
+static inline void apic_ext_print_public_header(Exports *exports) {
+    __impl_ext_print_public_header(exports, stdout);
+}
+
+static void __impl_ext_print_public_header(Exports *exports, FILE *out) {
+    fprintf(out, "// =============================================\n");
+    fprintf(out, "// Public API Header: %s\n", exports->name);
+    fprintf(out, "// Description: %s\n", exports->doc);
+    fprintf(out, "// =============================================\n\n");
+    fprintf(out, "#ifndef %s_PUBLIC_H\n", exports->name);
+    fprintf(out, "#define %s_PUBLIC_H\n\n", exports->name);
+
+    // Print includes
+    fprintf(out, "#include <stdint.h>\n\n");
+
+    // Print typedefs first
+    if (exports->typedef_count > 0) {
+        fprintf(out, "/* =============== Typedefs =============== */\n");
+        for (int i = 0; i < exports->typedef_count; i++) {
+            Typedef *t = exports->typedefs[i];
+            fprintf(out, "// %s\n", t->doc);
+            fprintf(out, "typedef %s %s;\n\n", t->type, t->name);
+        }
+    }
+
+    // Print enums
+    if (exports->enum_count > 0) {
+        fprintf(out, "/* ================ Enums ================ */\n");
+        for (int i = 0; i < exports->enum_count; i++) {
+            Enum *en = exports->enums[i];
+            fprintf(out, "// %s\n", en->doc);
+            fprintf(out, "typedef enum %s {\n", en->name);
+            for (int j = 0; j < en->count; j++) {
+                EnumEntry e = en->entries[j];
+                fprintf(out, "    %s = %d, // %s (%s)\n", e.name, e.value, e.str, e.doc);
+            }
+            fprintf(out, "} %s;\n\n", en->name);
+        }
+    }
+
+    // Print struct forward declarations
+    if (exports->struct_count > 0) {
+        fprintf(out, "/* ========== Forward Declarations ========= */\n");
+        for (int i = 0; i < exports->struct_count; i++) {
+            Struct *st = exports->structs[i];
+            fprintf(out, "typedef struct %s %s;\n", st->name, st->name);
+        }
+        fprintf(out, "\n");
+    }
+
+    // Print struct definitions
+    if (exports->struct_count > 0) {
+        fprintf(out, "/* ============== Structs ============== */\n");
+        for (int i = 0; i < exports->struct_count; i++) {
+            Struct *st = exports->structs[i];
+            fprintf(out, "// %s\n", st->doc);
+            fprintf(out, "struct %s {\n", st->name);
+            for (int j = 0; j < st->count; j++) {
+                Field f = st->fields[j];
+                fprintf(out, "    %s %s; // %s\n", f.type, f.name, f.doc);
+            }
+            fprintf(out, "};\n\n");
+        }
+    }
+
+    // Print unions
+    if (exports->union_count > 0) {
+        fprintf(out, "/* =============== Unions =============== */\n");
+        for (int i = 0; i < exports->union_count; i++) {
+            Union *u = exports->unions[i];
+            fprintf(out, "// %s\n", u->doc);
+            fprintf(out, "typedef union %s {\n", u->name);
+            for (int j = 0; j < u->count; j++) {
+                Field f = u->fields[j];
+                fprintf(out, "    %s %s; // %s\n", f.type, f.name, f.doc);
+            }
+            fprintf(out, "} %s;\n\n", u->name);
+        }
+    }
+
+    // Print lambdas (function pointer types)
+    if (exports->lambda_count > 0) {
+        fprintf(out, "/* ============== Lambdas ============== */\n");
+        for (int i = 0; i < exports->lambda_count; i++) {
+            Lambda *lam = exports->lambdas[i];
+            fprintf(out, "// %s\n", lam->doc);
+            fprintf(out, "typedef %s (*%s)(", lam->ret, lam->name);
+            for (int j = 0; j < lam->count; j++) {
+                Arg a = lam->args[j];
+                fprintf(out, "%s %s", a.type, a.name);
+                if (j < lam->count - 1) fprintf(out, ", ");
+            }
+            fprintf(out, ");\n\n");
+        }
+    }
+
+    // Print variables
+    if (exports->var_count > 0) {
+        fprintf(out, "/* ============= Variables ============= */\n");
+        for (int i = 0; i < exports->var_count; i++) {
+            Var *v = exports->vars[i];
+            fprintf(out, "// %s\n", v->doc);
+            fprintf(out, "extern %s %s;\n\n", v->type, v->name);
+        }
+    }
+
+    // Print functions
+    if (exports->func_count > 0) {
+        fprintf(out, "/* ============= Functions ============= */\n");
+        for (int i = 0; i < exports->func_count; i++) {
+            Func *func = exports->funcs[i];
+            fprintf(out, "// %s\n", func->doc);
+            fprintf(out, "%s %s(", func->ret, func->name);
+            for (int j = 0; j < func->count; j++) {
+                Arg a = func->args[j];
+                fprintf(out, "%s %s", a.type, a.name);
+                if (j < func->count - 1) fprintf(out, ", ");
+            }
+            fprintf(out, ");\n\n");
+        }
+    }
+
+    fprintf(out, "#endif // %s_PUBLIC_H\n", exports->name);
+}
+
+#endif // APIC_EXT_PRINT_PUBLIC_HEADER_H
--- test/basic_reflect.c
+++ test/basic_reflect.c
@@ -1,8 +1,16 @@
 #define APIC_REFLECT
 #include "basic_apic.h"
 #include "apic_ext_prettyprint.h"
+#include "apic_ext_print_public_header.h"
 
 int main() {
     apic_ext_prettyprint(&MyExports);
+
+    printf("\n----------------------------------------------------------\n");
+    printf("\n----------------------------------------------------------\n");
+    printf("\n----------------------------------------------------------\n\n\n");
+
+    apic_ext_print_public_header(&MyExports);
+
     return 0;
 }
